ae678d1b9555d880251b33f9089552f9c3a94f4f
Anaid-Ariwany <anaidariwany@gmail.com>
2026-03-17 14:42:02 +0300

    Deleted everything. Starting coding afresh.

--- js/app.js
+++
@@ -1,7 +0,0 @@
-/* select button to listen for click events */
-const addJobButton = document.querySelector('.addJob');
-
-addJobButton.addEventListener('click', () => {
-    const jobModal = new bootstrap.Modal(document.getElementById('jobModal'));
-    jobModal.show();
-});
